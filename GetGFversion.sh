0cfef1da3d703ffd410b99f79f6f75d0b4f5a746
SamueleGiuli <name.surname (at) gmail (dot) com>
2021-05-11 14:31:23 +0200

    Added GetGFversion.sh

---
+++ GetGFversion.sh
@@ -0,0 +1,16 @@
+#Getting gfortran version
+touch GF-version.txt
+GFVER=$(gfortran --version | head -n1)
+GFVER=${GFVER##* }
+
+echo $GFVER
+
+GF_CHAR=$( echo $GFVER | head -c 2  | tail -c 1)
+
+#Change if gfver>=10.x.x
+if(GF_CHAR==".")
+then
+    echo "gfortran version: $GFVER < 10.x.x"
+else
+    sed -i 's/-cpp -ffree-line-length-none/-cpp -ffree-line-length-none  -w -fallow-argument-mismatch/g' $1/CMakeLists.txt
+fi
