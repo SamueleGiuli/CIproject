82b4046c8b333b0e605b20d80bece6b0e6ab71c0
SamueleGiuli <name.surname (at) gmail (dot) com>
2021-05-11 14:58:34 +0200

    Update sed to CMakeLists.txt

--- GetGFversion.sh
+++ GetGFversion.sh
@@ -7,10 +7,11 @@ echo $GFVER
 
 GF_CHAR=$( echo $GFVER | head -c 2  | tail -c 1)
 echo "GFCHAR= $GF_CHAR"
+
 #Change if gfver>=10.x.x
 if [ "$GF_CHAR" = "." ]
 then
     echo "gfortran version: $GFVER < 10.x.x"
 else
-    sed -i 's/-cpp -ffree-line-length-none/-cpp -ffree-line-length-none  -w -fallow-argument-mismatch/g' $1/CMakeLists.txt
+    sed -i 's/-cpp -ffree-line-length-none/-cpp -ffree-line-length-none  -w -fallow-argument-mismatch/g' ./$1/CMakeLists.txt
 fi
