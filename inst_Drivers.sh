e21e4bd7574c71bfb00a6f82093b0faa249dee9f
SamueleGiuli <name.surname (at) gmail (dot) com>
2021-06-06 20:53:10 +0200

    MinorFix

--- inst_Drivers.sh
+++ inst_Drivers.sh
@@ -6,6 +6,15 @@ set -e
 #Sourcing .bashrc
 source ~/.bashrc
 
+#Sourcing .bashrc
+source ~/.bashrc
+SCIFORVER=$( ls ~/opt/scifor/gnu)
+source ~/opt/scifor/gnu/${SCIFORVER}/bin/scifor_config_user.sh
+DMFTVER=$( ls ~/opt/dmft_tools/gnu )
+source ~/opt/dmft_tools/gnu/${DMFTVER}/bin/dmft_tools_config_user.sh
+
+
+
 
 cd Driver-Database
 
